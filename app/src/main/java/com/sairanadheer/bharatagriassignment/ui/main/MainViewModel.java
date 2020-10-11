08e530d5de25898b0ce634e3e7b2cb9dc3f92cfd
ranadheer-ihs <ranadheer#ihscareers.com>
2020-10-11 19:38:42 +0530

    Initial commit

---
+++ app/src/main/java/com/sairanadheer/bharatagriassignment/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.sairanadheer.bharatagriassignment.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
